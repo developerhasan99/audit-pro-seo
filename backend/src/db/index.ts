c9a773157113a04b9759f65b7f36866c53032bf3
Mehedi Hasan <developerhasan99@gmail.com>
2025-12-26 16:45:05 +0600

    Updated commands on package json

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,7 +1,7 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { Pool } from 'pg';
-import * as schema from './schema';
-import config from '../config';
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import * as schema from "./schema";
+import config from "../config";
 
 const pool = new Pool({
   connectionString: config.database.url,
